cf0c0b874c5ea762db811b435d36e3e527765e1b
Paul Njuga <paulnjuga@gmail.com>
2023-08-04 01:23:21 +0300

    Refactor kwargs for initialization

--- models/base_model.py
+++ models/base_model.py
@@ -17,30 +17,28 @@ class BaseModel:
             *args: list of arguments
             **kwargs: dict of key-value arguments
         """
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "__class__":
-                    pass
-                else:
-                    self.__dict__[key] = kwargs[key]
-        else:
-            self.id = str(uuid. uuid4())
+        if not kwargs:
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
+        else:
+            kwargs["created_at"] = datetime.strptime(
+                kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f"
+            )
+            kwargs["updated_at"] = datetime.strptime(
+                kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f"
+            )
+            del kwargs["__class__"]
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns an informal and nicely printable string representation
         of a BaseModel instance."""
 
-        return "[{}] ({}) {}"\
-            .format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__
+        )
 
     def save(self):
         """Updates the public instance attribute updated_at
